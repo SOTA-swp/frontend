6b924fbfb51709ea7d4f53e65ca20b49515ffadc
Tooyaes <23fi005@ms.dendai.ac.jp>
2025-11-11 16:28:19 +0900

    CommonButtonを追加しました

--- app/dev/page.tsx
+++ app/dev/page.tsx
@@ -1,26 +1,38 @@
+import CommonButton from "@/components/CommonButton";
 import { Switch } from "@/components/ui/switch";
 import { UserInfoBlock } from "@/components/UserInfoBlock";
-
+import { SlArrowRight } from "react-icons/sl";
 
 export interface DevPageProps {
-    a: undefined;
+  a: undefined;
 }
 
-const DevPage: React.FC<DevPageProps> = ({ }) => {
-    if (process.env.NODE_ENV !== 'development') {
-        return <div>Not Found</div>;
-    }
+const DevPage: React.FC<DevPageProps> = ({}) => {
+  if (process.env.NODE_ENV !== "development") {
+    return <div>Not Found</div>;
+  }
 
-    return <div>
-        Dev Page
-        <Switch>
-            
-        </Switch>
-
-        <div className="flex">
-            <UserInfoBlock title="タイトル" sum={999} />
-        </div>
-    </div>;
-}
+  return (
+    <div>
+      Dev Page
+      <Switch></Switch>
+      <div className="flex">
+        <UserInfoBlock title="タイトル" sum={999} />
+      </div>
+      <div className="flex margin-top-20">
+        <CommonButton
+          size="md"
+          variant="primary"
+          icon={
+            <span>
+              <SlArrowRight />
+            </span>
+          }
+          text="Button"
+        />
+      </div>
+    </div>
+  );
+};
 
 export default DevPage;
---
+++ components/CommonButton.tsx
@@ -0,0 +1,64 @@
+import ComponentSizeType from "@/types/componentSize";
+import clsx from "clsx";
+import { Span } from "next/dist/trace";
+import React from "react";
+
+type VariantType = "primary" | "outline" | "disable" | "icon-only";
+
+function CommonButton({
+    size ="md",
+    variant = "primary",
+    icon,
+    text,
+    ...props
+}: {
+    size?:ComponentSizeType;
+    variant?:VariantType;
+    icon?:React.ReactNode;
+    text?:React.ReactNode;
+} & React.ButtonHTMLAttributes<HTMLButtonElement>) {
+    return (
+        <button
+            {...props}
+            className={clsx(
+                `
+                relative
+                flex
+                items-center
+                justify-items-center
+                rounded-xl
+                group
+                hover:brightness-90
+                active:brightness-80
+                transition
+                `,
+                size === "xs" && "p-0.5 text-[1rem]",
+                size === "sm" && "p-1 text-[1.25rem]",
+                size === "md" && "p-2 text-[1.5rem]",
+                size === "lg" && "p-2.5 text-[1.75rem]",
+                size === "xl" && "p-3 text-[2rem]",
+                variant === "primary" && "bg-primary text-paper",
+                variant === "outline" && "border border-primary bg-paper text-primary",
+                variant === "disable" &&
+                  "bg-shadow text-paper hover:brightness-100 active:brightness-100 cursor-not-allowed",
+                variant === "icon-only" && "bg-transparent text-text-secondary",
+                props.className
+            )}>
+            <div
+                className="
+                absolute
+                inset-0
+                rounded-full
+                bg-transparent
+                group-hover:bg-shadow/50
+                group-active:bg-shadow/70
+                transition
+            "
+            />
+            {text && <span className="ml-2">{text}</span>}
+            {icon && <span className="ml-15">{icon}</span>}
+        </button>
+    );
+}
+
+export default CommonButton;
