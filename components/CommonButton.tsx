5e7966142d1be3db20ebe1d4b3dc241d9c29c6d5
Tooyaes <23fi005@ms.dendai.ac.jp>
2025-11-18 13:36:09 +0900

    cvaにしました コメントアウト削除しました

--- components/CommonButton.tsx
+++ components/CommonButton.tsx
@@ -143,53 +143,5 @@ function CommonButton({
 }
 
 export default CommonButton;
-//     return (
-//         <button
-//             {...props}
-//             className={clsx(
-//                 `
-//                 relative
-//                 flex
-//                 items-center
-//                 justify-items-center
-//                 rounded-xl
-//                 group
-//                 active:scale-90
-//                 transition
-//                 `,
-//                 size === "xs" && "p-0.5 text-[1rem]",
-//                 size === "sm" && "p-1 text-[1.25rem]",
-//                 size === "md" && "p-2 text-[14px]",
-//                 size === "lg" && "p-2.5 text-[1.75rem]",
-//                 size === "xl" && "p-3 text-[2rem]",
-//                 variant === "contain" && 
-//                     clsx(
-//                         color === "primary" && `bg-primary text-paper hover:brightness-90`,
-//                         color === "gray" && `bg-gray text-paper hover:brightness-90`,
-//                         color === "accent" && `bg-accent text-paper hover:brightness-90`,
-//                         color === "error" && `bg-error text-paper hover:brightness-90`,
-//                     ),
-//                 variant === "outline" && 
-//                     clsx(
-//                         color === "primary" && `border border-primary bg-paper text-primary hover:brightness-90`,
-//                         color === "gray" && `border border-gray bg-paper text-gray hover:brightness-90`,
-//                         color === "accent" && `border border-accent bg-paper text-accent hover:brightness-90`,
-//                         color === "error" && `border border-error bg-paper text-error hover:brightness-90`,
-//                     ),
-//                 variant === "text" &&
-//                     clsx(
-//                         color == "primary" && "bg-background text-TextSecondary hover:bg-(--primary-thin) hover:text-primary active:brightness-100",
-//                         color == "gray" && "bg-background text-TextSecondary hover:bg-(--primary-thin) hover:text-primary active:brightness-100",
-//                         color == "accent" && "bg-background text-TextSecondary hover:bg-(--primary-thin) hover:text-primary active:brightness-100",
-//                         color == "error" && "bg-background text-TextSecondary hover:bg-(--primary-thin) hover:text-primary active:brightness-100",
-//                     ),
-//                 props.className
-//             )}>
-//             {text && <span className="ml-2">{text}</span>}
-//             {icon && <span className="ml-15">{icon}</span>}
-//         </button>
-//     );
-// }
 
-// export default CommonButton;
 
