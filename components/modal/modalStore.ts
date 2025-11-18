94bdfa9ce3abe9cae7df59b66c5d61869ebf8ebf
i-X-ce <iceci2804@gmail.com>
2025-11-18 20:07:25 +0900

    ✨ 開いているときにスクロールできないようにする

--- components/modal/modalStore.ts
+++ components/modal/modalStore.ts
@@ -19,6 +19,7 @@ export const useModalStore = create<ModalState>((set) => ({
   isOpen: false,
   payloadQueue: [],
   openModal: (content) => {
+    document.body.style.overflow = "hidden";
     set((state) => ({
       isOpen: true,
       payloadQueue: state.payloadQueue
@@ -27,12 +28,17 @@ export const useModalStore = create<ModalState>((set) => ({
     }));
   },
   closeModal: () => {
+    document.body.style.overflow = "";
     set({ isOpen: false });
   },
   shiftQueue: () => {
-    set((state) => ({
-      isOpen: state.payloadQueue.length > 1,
-      payloadQueue: state.payloadQueue.slice(1),
-    }));
+    set((state) => {
+      const newOpen = state.payloadQueue.length > 1;
+      document.body.style.overflow = "hidden";
+      return {
+        isOpen: newOpen,
+        payloadQueue: state.payloadQueue.slice(1),
+      };
+    });
   },
 }));
