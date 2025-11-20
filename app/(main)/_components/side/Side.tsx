704ef5514b55e79b233c1e43a68ca845feae7ec5
i-X-ce <iceci2804@gmail.com>
2025-11-21 02:59:06 +0900

    🔥 いらないimportを削除

--- app/(main)/_components/side/Side.tsx
+++ app/(main)/_components/side/Side.tsx
@@ -10,7 +10,7 @@ import PATH from "@/consts/PATH";
 import { SIDE_VIEWS, SIDE_VIEWS_TYPE, useSideStore } from "./sideStore";
 import { MAIN_PAGE_IDs } from "../../_consts/MAIN_PAGE_IDs";
 import React from "react";
-import { useParams, usePathname } from "next/navigation";
+import { useParams } from "next/navigation";
 import { motion } from "motion/react";
 import clsx from "clsx";
 import HEADER_HEIGHT from "../../_consts/HEADER_HIGHT";
@@ -53,7 +53,6 @@ const ITEM_DATA: {
 
 function Side() {
   const { userId } = useParams();
-  // const pathName = usePathname();
   const { currentView } = useSideStore();
 
   return (
