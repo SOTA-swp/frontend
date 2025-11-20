c5a659161ef8c91b2ce8b179f55a557f5436c8aa
i-X-ce <iceci2804@gmail.com>
2025-11-21 00:13:49 +0900

    🎨 ItemDataの定義

--- components/side/Side.tsx
+++ components/side/Side.tsx
@@ -8,21 +8,50 @@ import {
 import CommonButton from "../CommonButton";
 import { HEADER_HEIGHT } from "../CommonHeader";
 import PATH from "@/consts/PATH";
-import { usePathname } from "next/navigation";
+import { SIDE_VIEWS, SIDE_VIEWS_TYPE, useSideStore } from "./sideStore";
+
+const ITEM_DATA: {
+  label: string;
+  icon: React.ReactNode;
+  path: string | ((userId: string) => string);
+  sideViewType: SIDE_VIEWS_TYPE;
+}[] = [
+  {
+    label: "ユーザー",
+    icon: <MdAccountCircle />,
+    path: PATH.USER,
+    sideViewType: SIDE_VIEWS.USER,
+  },
+  {
+    label: "作った計画",
+    icon: <MdAirplanemodeActive />,
+    path: PATH.USER,
+    sideViewType: SIDE_VIEWS.PLANS,
+  },
+  {
+    label: "お気に入り",
+    icon: <MdFavorite />,
+    path: PATH.USER,
+    sideViewType: SIDE_VIEWS.FAVORITES,
+  },
+  {
+    label: "検索",
+    icon: <MdSearch />,
+    path: PATH.SEARCH,
+    sideViewType: SIDE_VIEWS.SEARCH,
+  },
+];
 
 function Side() {
-  const path = usePathname();
+  const { currentView } = useSideStore();
 
   return (
     <div className="shrink-0">
       <ul
         className="sticky rounded-md w-[290px] flex flex-col gap-4 py-4 px-4 border border-primary bg-paper"
         style={{ top: HEADER_HEIGHT + 20 }}>
-        <CommonButton
-          variant="text"
-          icon={<MdAccountCircle />}
-          href={PATH.HOME}>
-          ユーザー{path}
+        <CommonButton variant="text" icon={<MdAccountCircle />} href={PATH.TOP}>
+          ユーザー
         </CommonButton>
         <CommonButton variant="text" icon={<MdAirplanemodeActive />}>
           作った計画
