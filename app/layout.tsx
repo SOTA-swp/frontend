dad6f09fe47b6a833ad610f08afadbdfb4246a7f
i-X-ce <iceci2804@gmail.com>
2025-11-21 18:50:03 +0900

    🔥 いらないimportを削除

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import CommonHeader from "@/app/(main)/_components/CommonHeader";
 import Modal from "@/components/modal/Modal";
 
 const geistSans = Geist({
