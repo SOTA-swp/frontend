ebbb462738fe66bb9113e6d5510c1018b77f221c
i-X-ce <iceci2804@gmail.com>
2025-11-23 18:46:44 +0900

    🤡 userDataのモックデータを返す関数を追加

--- app/(main)/user/[userId]/page.tsx
+++ app/(main)/user/[userId]/page.tsx
@@ -1,9 +1,31 @@
 import { MAIN_PAGE_IDs } from "../../_consts/MAIN_PAGE_IDs";
 import UserView from "../../_components/UserView";
 import PlanView from "../../_components/PlanView";
+import UserType from "@/types/user";
 
 export interface UserPageProps {
   params: { userId: Promise<string> };
+}
+
+async function createUserViewMockData(userId: string): Promise<
+  UserType & {
+    favoritesCount: number;
+    createdCount: number;
+  }
+> {
+  await new Promise((resolve) => setTimeout(resolve, 500));
+
+  return {
+    id: userId,
+    googleUserId: "??",
+    name: `ユーザー${userId}`,
+    email: `${userId}@mail.com`,
+    picture: "/mock/img/user.png",
+    createdAt: "2025-11-23T12:00:00.000Z",
+    updatedAt: "2025-11-23T12:00:00.000Z",
+    favoritesCount: 999,
+    createdCount: 999,
+  };
 }
 
 const UserPage: React.FC<UserPageProps> = async ({ params }) => {
@@ -11,7 +33,8 @@ const UserPage: React.FC<UserPageProps> = async ({ params }) => {
 
   return (
     <div>
-      <UserView userData={undefined} />
+      {/* TODO: 実際のAPIが完成したら置き換える */}
+      <UserView userData={await createUserViewMockData(await userId)} />
       <PlanView viewId={MAIN_PAGE_IDs.PLANS} plans={[]} />
       <PlanView viewId={MAIN_PAGE_IDs.FAVORITES} plans={[]} />
       {/* {Object.values(MAIN_PAGE_IDs).map((id) => (
