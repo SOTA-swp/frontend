204d09fd19dbd4c78a343b1adaa71dad61a46e42
i-X-ce <iceci2804@gmail.com>
2025-11-23 19:43:28 +0900

    💄 背景をしっかり伸ばす

--- app/(main)/_components/UserView.tsx
+++ app/(main)/_components/UserView.tsx
@@ -21,44 +21,50 @@ function UserView({ userData }: UserViewProps) {
   return (
     <section
       id={MAIN_PAGE_IDs.USER}
-      className="relative flex flex-col gap-6 w-full p-8 "
+      className="relative "
       style={{ scrollMarginTop: VIEW_TOP_MARGIN }}>
-      <div className="absolute -z-10 inset-0 bg-paper inset-shadow-2 rounded-lg inset-shadow-sm border border-primary" />
-      <div className="flex justify-between items-center">
-        <div className="flex items-center gap-4">
-          <div className="flex items-center rounded-full border-2 border-accent shrink-0">
-            {
-              // eslint-disable-next-line @next/next/no-img-element, jsx-a11y/alt-text
-              <img src={userData.picture} className="w-20 h-20" />
-            }
+      <div className="absolute -z-10 w-[200dvw] h-full bg-paper inset-shadow-2 rounded-lg inset-shadow-sm border border-primary" />
+
+      <div className="relative flex flex-col gap-6 w-full p-8">
+        <div className="flex justify-between items-center">
+          <div className="flex items-center gap-4">
+            <div className="flex items-center rounded-full border-2 border-accent shrink-0">
+              {
+                // eslint-disable-next-line @next/next/no-img-element, jsx-a11y/alt-text
+                <img src={userData.picture} className="w-20 h-20" />
+              }
+            </div>
+            <div>
+              <h1 className="text-2xl font-bold">{userData.name}</h1>
+              <p className="text-text-secondary">
+                {createDate.toLocaleDateString()}から利用しています
+              </p>
+            </div>
           </div>
-          <div>
-            <h1 className="text-2xl font-bold">{userData.name}</h1>
-            <p className="text-text-secondary">
-              {createDate.toLocaleDateString()}から利用しています
-            </p>
+          <div className="flex gap-8">
+            <GrowIconButton icon={<MdAdd />} title="新規作成">
+              新規作成
+            </GrowIconButton>
+            <GrowIconButton icon={<MdSearch />} title="検索">
+              検索
+            </GrowIconButton>
+            <GrowIconButton icon={<MdNotifications />} title="通知">
+              通知
+            </GrowIconButton>
+            <GrowIconButton icon={<MdLogout />} title="ログアウト">
+              ログアウト
+            </GrowIconButton>
           </div>
         </div>
-        <div className="flex gap-8">
-          <GrowIconButton icon={<MdAdd />} title="新規作成">
-            新規作成
-          </GrowIconButton>
-          <GrowIconButton icon={<MdSearch />} title="検索">
-            検索
-          </GrowIconButton>
-          <GrowIconButton icon={<MdNotifications />} title="通知">
-            通知
-          </GrowIconButton>
-          <GrowIconButton icon={<MdLogout />} title="ログアウト">
-            ログアウト
-          </GrowIconButton>
+        <div className="flex gap-4">
+          <UserInfoBlock title={"作った計画"} sum={userData.createdCount} />
+          <UserInfoBlock title={`${PROJECT_NAME}歴`} sum={0} />
+          <UserInfoBlock
+            title={"いいねされた数"}
+            sum={userData.favoritesCount}
+          />
+          <UserInfoBlock title={"いいねした数"} sum={userData.favoredCount} />
         </div>
-      </div>
-      <div className="flex gap-2">
-        <UserInfoBlock title={"作った計画"} sum={userData.createdCount} />
-        <UserInfoBlock title={`${PROJECT_NAME}歴`} sum={0} />
-        <UserInfoBlock title={"いいねされた数"} sum={userData.favoritesCount} />
-        <UserInfoBlock title={"いいねした数"} sum={userData.favoredCount} />
       </div>
     </section>
   );
--- components/UserInfoBlock.tsx
+++ components/UserInfoBlock.tsx
@@ -6,11 +6,11 @@ type data = {
 export function UserInfoBlock(props: data) {
   const { title, sum } = props;
   return (
-    <div className="flex flex-col items-center justify-center bg-white rounded-lg shadow-md p-2 min-w-[150px]">
+    <div className="flex flex-col justify-center bg-white rounded-lg shadow-md p-4 min-w-[150px] flex-1">
       <h3 className="text-text-secondary text-lg font-bold">{title}</h3>
-      <p className="text-primary text-5xl font-bold mt-2">
+      <p className="text-primary text-5xl font-bold mt-2 text-center">
         {sum}
-        <span className="text-sm text-primary">個</span>
+        <span className="text-sm text-primary ">個</span>
       </p>
     </div>
   );
