be6e01b40ca029dfcd37ad6624f3e70678c9bd70
i-X-ce <iceci2804@gmail.com>
2025-11-25 20:48:25 +0900

    ✨ 検索ページの追加

--- app/(main)/search/page.tsx
+++ app/(main)/search/page.tsx
@@ -1,9 +1,25 @@
+import { getPlans } from "../actions";
+import SearchField from "./_components/SearchField";
+import SearchPlanView from "./_components/SearchPlanView";
+
 export interface SearchPageProps {
-    a: undefined;
+  searchPrams: { q?: string };
 }
 
-const SearchPage: React.FC<SearchPageProps> = ({ }) => {
-    return <div>Search Page</div>;
-}
+const SearchPage: React.FC<SearchPageProps> = async ({ searchPrams }) => {
+  const q = searchPrams?.q || "";
+
+  return (
+    <main className="flex-1">
+      <SearchField />
+      <SearchPlanView
+        initialPlans={{
+          popularPlans: await getPlans(q, 0),
+          newPlans: await getPlans(q, 0),
+        }}
+      />
+    </main>
+  );
+};
 
 export default SearchPage;
