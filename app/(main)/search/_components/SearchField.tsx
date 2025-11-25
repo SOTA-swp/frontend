9ce0486292faef519e5ea0bf6d6fb9f8f7c9a037
i-X-ce <iceci2804@gmail.com>
2025-11-25 17:45:25 +0900

    ✨ 検索バーの追加

---
+++ app/(main)/search/_components/SearchField.tsx
@@ -0,0 +1,44 @@
+"use client";
+import TextField from "@/components/TextField";
+import { useRef, useState } from "react";
+import MainViewController from "../../_components/MainViewController";
+import { SIDE_VIEWS } from "../../_components/side/sideStore";
+import { useSearchParams } from "next/navigation";
+import IconButton from "@/components/IconButton";
+import { MdSearch } from "react-icons/md";
+
+function SearchField() {
+  const searchParams = useSearchParams();
+  const ref = useRef(null);
+  const [term, setTerm] = useState(searchParams.get("q") || "");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTerm(e.target.value);
+  };
+
+  return (
+    <form ref={ref} className="w-full max-w-[700px] flex items-center gap-2">
+      <MainViewController
+        ref={ref}
+        viewId={SIDE_VIEWS.SEARCH}
+        rootMargin="0px 0px 0px 0px"
+      />
+      <TextField
+        value={term}
+        onChange={handleChange}
+        label="q"
+        labelName="検索"
+        placeholder="沖縄旅行"
+        fullWidth
+      />
+      <IconButton
+        type="submit"
+        title="検索"
+        icon={<MdSearch />}
+        variant={"iconOnly"}
+      />
+    </form>
+  );
+}
+
+export default SearchField;
