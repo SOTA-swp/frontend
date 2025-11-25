d100f991b8a95b9de8acb0ae83f725baff4f4d83
i-X-ce <iceci2804@gmail.com>
2025-11-25 20:48:01 +0900

    🎨 API系統をserverActionsに分離

--- app/(main)/user/[userId]/page.tsx
+++ app/(main)/user/[userId]/page.tsx
@@ -1,42 +1,10 @@
 import { MAIN_PAGE_IDs } from "../../_consts/MAIN_PAGE_IDs";
 import UserView from "../../_components/UserView";
 import PlanView from "../../_components/PlanView";
-import UserType, { createMockUser } from "@/types/user";
-import { createMockPlan } from "@/types/plan";
+import { getPlans, getUserData } from "../../actions";
 
 export interface UserPageProps {
   params: { userId: Promise<string> };
-}
-
-async function createUserViewMockData(userId: string): Promise<
-  UserType & {
-    favoritesCount: number;
-    favoredCount: number;
-    createdCount: number;
-  }
-> {
-  await new Promise((resolve) => setTimeout(resolve, 500));
-
-  return {
-    id: userId,
-    googleUserId: "??",
-    name: `ユーザー${userId}`,
-    email: `${userId}@mail.com`,
-    picture: "/mock/img/user.png",
-    createdAt: "2025-11-23T12:00:00.000Z",
-    updatedAt: "2025-11-23T12:00:00.000Z",
-    favoritesCount: 999,
-    favoredCount: 999,
-    createdCount: 999,
-  };
-}
-
-async function createPlanViewMockData() {
-  await new Promise((resolve) => setTimeout(resolve, 500));
-  return Array.from({ length: 2 }).map((_, i) => ({
-    planData: { favorites: 999, ...createMockPlan(i + 1) },
-    userData: createMockUser(i + 1),
-  }));
 }
 
 const UserPage: React.FC<UserPageProps> = async ({ params }) => {
@@ -45,14 +13,14 @@ const UserPage: React.FC<UserPageProps> = async ({ params }) => {
   return (
     <main className="relative flex-1 ">
       {/* TODO: 実際のAPIが完成したら置き換える */}
-      <UserView userData={await createUserViewMockData(await userId)} />
+      <UserView userData={await getUserData(await userId)} />
       <PlanView
         viewId={MAIN_PAGE_IDs.PLANS}
-        plans={await createPlanViewMockData()}
+        plans={(await getPlans("", 0)).planData}
       />
       <PlanView
         viewId={MAIN_PAGE_IDs.FAVORITES}
-        plans={await createPlanViewMockData()}
+        plans={(await getPlans("", 0)).planData}
       />
 
       {/* お気に入りの下の余白を作る */}
