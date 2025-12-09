705da1d4ceddbcb97efc1ac5f54b7883506a76ce
hayato-oshima <23fi015@ms.dendai.ac.jp>
2025-12-09 16:43:12 +0900

    icon の追加

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,7 +4,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <div className="fixed top-4 left-4">
-        <CommonButton variant="text" color="gray" size="md">＜ 戻る</CommonButton>
+        <CommonButton variant="text" color="gray" size="md" icon>＜ 戻る</CommonButton>
       </div>
       <div>{children}</div>
     </>
