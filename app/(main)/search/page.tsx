53ee71b53fc8006c0601d959e975cf4c16becffc
i-X-ce <iceci2804@gmail.com>
2025-12-09 20:01:49 +0900

    🐛 buildが通らなかったのでSuspendで囲った

--- app/(main)/search/page.tsx
+++ app/(main)/search/page.tsx
@@ -1,32 +1,35 @@
+import { Suspense } from "react";
 import { getPlans } from "../actions";
 import SearchField from "./_components/SearchField";
 import SearchPlanView from "./_components/SearchPlanView";
 
 export interface SearchPageProps {
-  searchPrams: { q?: string };
+  searchPrams: Promise<{ q?: string }>;
 }
 
 const SearchPage: React.FC<SearchPageProps> = async ({ searchPrams }) => {
-  const q = searchPrams?.q || "";
+  const q = (await searchPrams)?.q || "";
 
   const popularPlans = await getPlans(q, 0);
   const newPlans = await getPlans(q, 0);
 
   return (
     <main className="flex-1">
-      <SearchField />
-      <SearchPlanView
-        initialPlans={{
-          popularPlans: {
-            size: popularPlans.size,
-            planData: popularPlans.planData,
-          },
-          newPlans: {
-            size: newPlans.size,
-            planData: newPlans.planData,
-          },
-        }}
-      />
+      <Suspense fallback={<div>Loading search view...</div>}>
+        <SearchField />
+        <SearchPlanView
+          initialPlans={{
+            popularPlans: {
+              size: popularPlans.size,
+              planData: popularPlans.planData,
+            },
+            newPlans: {
+              size: newPlans.size,
+              planData: newPlans.planData,
+            },
+          }}
+        />
+      </Suspense>
     </main>
   );
 };
