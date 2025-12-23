1ce195514671c7011478b7a75d68d427832a25b4
i-X-ce <iceci2804@gmail.com>
2025-12-23 19:46:45 +0900

    🏷️ ノードの追加に必要な要素をまとめて置く

--- app/plans/_components/NodeView/index.tsx
+++ app/plans/_components/NodeView/index.tsx
@@ -1,12 +1,12 @@
 import { ReactNode, useState } from "react";
 import NodeThree from "./NodeThree";
 import ViewWrapper from "../ViewWrapper";
-import { MdDirectionsCar, MdFolderOpen, MdLocationPin } from "react-icons/md";
 import IconButton from "@/components/IconButton";
 import AddButton from "../AddButton";
 import { usePlanStore } from "../../_store/hook";
 import { NODE_TYPES, NodeType } from "@/types/node";
 import { createNode } from "../../_util/createNode";
+import { NODE_TYPE_ITEMS } from "./NODE_ITEMS";
 
 function NodeViewAddButton() {
   const [addMode, setAddMode] = useState<NodeType>(NODE_TYPES.PROCESS);
@@ -21,38 +21,18 @@ function NodeViewAddButton() {
     addNode(newNode);
   };
 
-  const addButtonChildrenData: {
-    mode: NodeType;
-    title: string;
-    icon: ReactNode;
-  }[] = [
-    {
-      mode: NODE_TYPES.PROCESS,
-      title: "プロセス",
-      icon: <MdFolderOpen />,
-    },
-    {
-      mode: NODE_TYPES.MOVE,
-      title: "移動",
-      icon: <MdDirectionsCar />,
-    },
-    {
-      mode: NODE_TYPES.LOCATION,
-      title: "ロケーション",
-      icon: <MdLocationPin />,
-    },
-  ];
-  const addButtonChildrenNodes: ReactNode[] = addButtonChildrenData.map(
-    (data) => (
-      <IconButton
-        {...data}
-        key={data.mode}
-        onClick={() => handleModeChange(data.mode)}
-        size={"md"}
-        variant={addMode === data.mode ? "contain" : "outline"}
-      />
-    )
-  );
+  const addButtonChildrenNodes: ReactNode[] = Object.entries(
+    NODE_TYPE_ITEMS
+  ).map(([mode, { title, icon }]) => (
+    <IconButton
+      key={mode}
+      title={title}
+      icon={icon}
+      onClick={() => handleModeChange(mode as NodeType)}
+      size={"md"}
+      variant={addMode === mode ? "contain" : "outline"}
+    />
+  ));
   return (
     <AddButton onClick={handleAddNode} childButtons={addButtonChildrenNodes} />
   );
