93dc548df7dbe414e321eb7dc4bd55fea7253b99
hayato-oshima <23fi015@ms.dendai.ac.jp>
2025-12-23 13:50:56 +0900

    label, form, typeの追加

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -15,30 +15,38 @@ const TopPage: React.FC<TopPageProps> = ({}) => {
         </CommonText>
       </div>
 
-      <div className="flex flex-col gap-16 items-center">
+      <form className="flex flex-col gap-16 items-center">
         <div className="flex flex-col items-center gap-12 w-[500px]">
           <TextField
-            label="名前"
+            label="name"
+            labelName="名前"
             placeholder="名前を入力"
             fullWidth
-          ></TextField>
+          />
           <TextField
-            label="メールアドレス"
+            label="mail_address"
+            labelName="メールアドレス"
             placeholder="メールアドレスを入力"
             fullWidth
-          ></TextField>
+          />
           <TextField
-            label="パスワード"
+            label="password"
+            labelName="パスワード"
             placeholder="パスワードを入力"
             fullWidth
-          ></TextField>
+          />
         </div>
         <div className="flex justify-center">
-          <CommonButton variant="contain" color="primary" size="lg">
+          <CommonButton
+            variant="contain"
+            color="primary"
+            size="lg"
+            type="submit"
+          >
             新規作成
           </CommonButton>
         </div>
-      </div>
+      </form>
     </div>
   );
 };
