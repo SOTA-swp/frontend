7a29a40de8ef8d883e629c1e6029d0a7b6c9863a
i-X-ce <iceci2804@gmail.com>
2025-12-29 19:11:30 +0900

    ✨ fetchWrapperを定義して共通で使いまわす

---
+++ utils/fetchWrapper.ts
@@ -0,0 +1,33 @@
+export const fetchWrapper = {
+  get: async <T>(url: string, options: RequestInit = {}) => {
+    const response = await fetch(url, {
+      ...options,
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        ...(options.headers || {}),
+      },
+    });
+    return response;
+  },
+  post: async (
+    url: string,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    body: any,
+    useServer: boolean = false,
+    options: RequestInit = {}
+  ) => {
+    url = `${useServer ? process.env.API_ROUTE : ""}${url}`;
+    const response = await fetch(url, {
+      ...options,
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        ...(options.headers || {}),
+      },
+      body: JSON.stringify(body),
+    });
+
+    return response;
+  },
+};
