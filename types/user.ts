73d76a106d541ac0011a7bc69e7b600a105511fb
This-is-UserNamee <so1031eg@gmail.com>
2026-01-05 23:50:57 +0900

    usernameを追加

--- types/user.ts
+++ types/user.ts
@@ -3,7 +3,7 @@ import z from "zod";
 export const UserSchema = z.object({
   id: z.string(),
   googleUserId: z.string(),
-  name: z
+  username: z
     .string()
     .min(1, "ユーザー名は1文字以上")
     .max(50, "ユーザー名は50文字以下"),
@@ -18,7 +18,7 @@ export type User = z.infer<typeof UserSchema>;
 export const createMockUser = (num: number = 0): User => ({
   id: `mock-user-id${num}`,
   googleUserId: "mock-google-user-id",
-  name: `モックユーザー${num}`,
+  username: `モックユーザー${num}`,
   email: `mock-mail${num}@gmail.com`,
   picture: "/mock/img/user.png",
   createdAt: "2025-11-09T12:00:00.000Z",
