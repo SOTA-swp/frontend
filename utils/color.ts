b3045f02d6e99a1350aca4eca1469ee706d8075b
i-X-ce <iceci2804@gmail.com>
2026-01-07 21:49:38 +0900

    🎨 hashがもっと飛び散るように変更

--- utils/color.ts
+++ utils/color.ts
@@ -1,7 +1,11 @@
 export const getHueFromString = (str: string, min = 0, max = 360): number => {
-  const hash = Array.from(str).reduce(
+  let hash = Array.from(str).reduce(
     (acc, char) => (acc << 5) - acc + char.charCodeAt(0),
     0
   );
+  hash |= 0;
+  hash = Math.imul(hash ^ (hash >>> 16), 0x85ebca6b);
+  hash = Math.imul(hash ^ (hash >>> 13), 0xc2b2ae35);
+  hash ^= hash >>> 16;
   return (hash % (max - min)) + min;
 };
