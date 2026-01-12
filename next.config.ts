3237b3d23de7fdc791f7c9c6fa405edfe3cc8a74
This-is-UserNamee <so1031eg@gmail.com>
2026-01-12 19:11:48 +0900

    config を修正

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:4000/api/:path*",
+        destination: "https://monorepo-b5st.onrender.com/api/:path*",
       },
     ];
   },
